ee3d171cdff14236c69cbee54fe2c39acc06b0af
Dastan Abylkhassov <abylhasov@gmail.com>
2020-01-31 16:18:59 +0600

    User CRUD finished

---
+++ resources/views/users/edit.blade.php
@@ -0,0 +1,35 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="card">
+            <div class="card-header">
+                Users
+            </div>
+            <div class="card-body">
+                <form action="{{ route('users.update', $user) }}" method="post">
+                    @csrf
+                    @method('put')
+                    <div class="form-group">
+                        <label for="name">Name</label>
+                        <input name="name" type="text" class="form-control" id="name" placeholder="Enter name" required
+                               value="{{ $user->name }}">
+                    </div>
+                    <div class="form-group">
+                        <label for="email">Email</label>
+                        <input name="email" type="email" class="form-control" id="email" placeholder="Enter email" required
+                               value="{{ $user->email }}">
+                    </div>
+                    <div class="form-group">
+                        <label for="password">Password</label>
+                        <input name="password" type="password" class="form-control" id="password" placeholder="New password">
+                        <small id="passwordHelp" class="form-text text-muted">
+                            If you don't want change old password do not fill this field
+                        </small>
+                    </div>
+                    <button type="submit" class="btn btn-primary">Send</button>
+                </form>
+            </div>
+        </div>
+    </div>
+@endsection
